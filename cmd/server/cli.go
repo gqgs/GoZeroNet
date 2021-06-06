f80d8df12d4df598a96f7b30279f1d4b1b52a38c
Guilherme <gqgs@protonmail.com>
2021-06-06 08:50:43 -0300

    server: add flags to configure port

--- cmd/server/cli.go
+++ cmd/server/cli.go
@@ -1,12 +1,27 @@
 package server
 
-import "github.com/urfave/cli/v2"
+import (
+	"github.com/gqgs/go-zeronet/pkg/config"
+	"github.com/urfave/cli/v2"
+)
 
 func NewCommand() *cli.Command {
 	return &cli.Command{
 		Name:  "server",
 		Usage: "Start file and UI servers",
+		Flags: []cli.Flag{
+			&cli.IntFlag{
+				Name:  "file_server_port",
+				Value: config.FileServer.Port,
+			},
+			&cli.IntFlag{
+				Name:  "ui_server_port",
+				Value: config.UIServer.Port,
+			},
+		},
 		Action: func(c *cli.Context) error {
+			config.FileServer.Port = c.Int("file_server_port")
+			config.UIServer.Port = c.Int("ui_server_port")
 			return serve(c.Context)
 		},
 	}
